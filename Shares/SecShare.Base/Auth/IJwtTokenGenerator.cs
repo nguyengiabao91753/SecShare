83f7f123ad3a09a41055c0775a601a9ff59ce762
Bao <nguyengiabao91753@gmail.com>
2025-09-29 15:37:17 +0700

    service sinh Token

---
+++ Shares/SecShare.Base/Auth/IJwtTokenGenerator.cs
@@ -0,0 +1,12 @@
+﻿using SecShare.Core.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecShare.Base.Auth;
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(ApplicationUser user);
+}
