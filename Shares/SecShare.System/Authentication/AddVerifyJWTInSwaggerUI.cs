33ef3fd0c0bae44cdb93c8e31d51db46eca1f308
Bao <nguyengiabao91753@gmail.com>
2025-10-06 11:16:27 +0700

    Cấu hình chung Verift JWT trên Swagger UI

---
+++ Shares/SecShare.System/Authentication/AddVerifyJWTInSwaggerUI.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecShare.SystemConfig.Authentication;
+public static class AddVerifyJWTInSwaggerUI
+{
+    public static WebApplicationBuilder AddSwaggerWithJWT(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "SecShare API",
+                Version = "v1"
+            });
+
+            // Cấu hình Bearer Token
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Enter JWT token with Bearer prefix. Example: Bearer {token}",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            // Thêm yêu cầu bảo mật
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+        });
+
+        return builder;
+    }
+}
