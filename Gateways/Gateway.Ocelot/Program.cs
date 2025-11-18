4d246360e51799c2153fa8eb408bc3059510034f
Bao <nguyengiabao91753@gmail.com>
2025-11-18 18:43:30 +0700

    Tạo gateway

--- Gateways/Gateway.Ocelot/Program.cs
+++ Gateways/Gateway.Ocelot/Program.cs
@@ -1,8 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Ocelot configuration
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+// Add Ocelot
+builder.Services.AddOcelot();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,11 +26,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseOcelot().Wait();
 
 app.Run();
